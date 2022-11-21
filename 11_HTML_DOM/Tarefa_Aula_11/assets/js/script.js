92be6cb7ac01cefcf186cd558c8687ab112666b1
William Moura <williamonte@gmail.com>
2022-11-21 13:51:17 -0300

    update

--- 11_HTML_DOM/Tarefa_Aula_11/assets/js/script.js
+++ 11_HTML_DOM/Tarefa_Aula_11/assets/js/script.js
@@ -1,45 +1,40 @@
 function validaForm(){
-    let validahora = document.forms['agendar']['hora'].value;//Pega um conteudo que está dentro de uma célula de formulário.
-    let validaNomeDono = document.forms['agendar']['nomeDono'].value;
-    let validaNomeCachorro = document.forms['agendar']['nomeCachorro'].value;
-    let validaTel = document.forms['agendar']['tel'].value;
-    let validaData = document.forms['agendar']['data'].value;
-    let validaObs = document.forms['agendar']['observacao'].value;
+    let validahora = document.forms['agendar']['horaMarcada'].value;//Pega um conteudo que está dentro de uma célula de formulário.
+    let validaNomeDono = document.forms['agendar']['nomeTutor'].value;
+    let validaNomeCachorro = document.forms['agendar']['nomeCao'].value;
+    let validaTel = document.forms['agendar']['telef'].value;
+    let validaData = document.forms['agendar']['dataAgenda'].value;
 
     //Condicional para ver se os campos, do "form", está vazio.
     if(validahora == ""){
-        alert('O campo "Codigo" deve ser preenchido!')
+        alert('O campo "Horário" deve ser preenchido!')
         return false;
     }
     if(validaNomeDono == ""){
-        alert('O campo "Data" deve ser preenchido!')
+        alert('O campo "Nome do tutor" deve ser preenchido!')
         return false;
     }
     if(validaNomeCachorro == ""){
-        alert('O campo "Título" deve ser preenchido!')
+        alert('O campo "Nome do cachorro" deve ser preenchido!')
         return false;
     }
     if(validaTel == ""){
-        alert('O campo "Diretor" deve ser preenchido!')
+        alert('O campo "Telefone" deve ser preenchido!')
         return false;
     }
     if(validaData == ""){
-        alert('O campo "Ator/atriz" deve ser preenchido!')
-        return false;
-    }
-    if(validaObs == ""){
-        alert('O campo "Nota" deve ser preenchido!')
+        alert('O campo "Data" deve ser preenchido!')
         return false;
     }
 }
 
 function insere(){
-    let hora = document.forms['agendar']['hora'].value;
-    let nomeDono = document.forms['agendar']['nomeDono'].value;
-    let nomeCachorro = document.forms['agendar']['nomeCachorro'].value;
-    let tel = document.forms['agendar']['tel'].value;
-    let data = document.forms['agendar']['data'].value;
-    let observacao = document.forms['agendar']['observacao'].value;
+    let hora = document.forms['agendar']['horaMarcada'].value;
+    let nomeDono = document.forms['agendar']['nomeTutor'].value;
+    let nomeCachorro = document.forms['agendar']['nomeCao'].value;
+    let tel = document.forms['agendar']['telef'].value;
+    let data = document.forms['agendar']['dataAgenda'].value;
+    let observacao = document.forms['agendar']['obs'].value;
 
     let inserir = window.document.getElementById('inserirTabela');//Tudo que for colocado aqui, será colocado dentro do respectivo "id".
 
