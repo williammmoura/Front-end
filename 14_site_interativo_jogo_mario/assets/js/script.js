1940389544ef753f0af8a564f667ae8138772342
William Monte de Moura <williamonte@gmail.com>
2023-01-07 23:55:25 -0300

    score
    Definido o botão "jump" e disparo do "score"

--- 14_site_interativo_jogo_mario/assets/js/script.js
+++ 14_site_interativo_jogo_mario/assets/js/script.js
@@ -1,17 +1,26 @@
+//Constantes que serão usadas.
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const score = document.querySelector('#score');
 const gameOver = document.querySelector('#gameOver');
 
 
+/*************************************************************************/
+
+
 /*Arrow function para ajustar o pulo do Mario*/
 const jump = () => {
-    mario.classList.add('jump');
+    mario.classList.add('jump');//Adicionando a classe "jump".
 
+    /*"setTimeut" vai receber uma função "arrow" e o tempo da animação.*/
     setTimeout(()=>{
         mario.classList.remove('jump');
     }, 500);
 }
+
+
+/*************************************************************************/
+
 
 /*Função para testar a posição do Mario em relação ao cano.*/
 const loop = setInterval(()=>{
@@ -21,10 +30,13 @@ const loop = setInterval(()=>{
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
     /*Condição (if) para verificar a posição do Maria em relação ao cano*/
-    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 112){
-        pipe.style.animation = 'none';
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
+        
+        //Parando a animação na posição 120.
+        pipe.style.animation = 'none'; 
         pipe.style.left = `${pipePosition}px`;
 
+        //Verificando o instante em que o Maria encostou no cano.
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
 
@@ -38,28 +50,29 @@ const loop = setInterval(()=>{
 
 }, 10);
 
-document.addEventListener('keydown', jump);
+
+/************************************************************************/
 
 
+//Definindo o botão do teclado em que será feito o "jump"
+document.addEventListener('keydown', (start) => {
+    if((start.code === "ArrowUp") || (start.code === "Space")){
+        jump();
+    }
+});
 
+
+/************************************************************************/
+
+
+//Score
 //Declaração de variáveis para o "Score"
 let interval = null;
 let playerScore = 0;//Contador
-
 //Função para o "Score".
 let scoreCounter = ()=>{
     playerScore++;
     score.innerHTML=`Score - <b>${playerScore}</b>`;
-} 
-
-//Start Game.
-window.addEventListener('keydown', (start)=>{
-    //Definindo o botão para iniciar o contador (playerScore).
-    if(start.code == 'Space'){
-        gameOver.style.display = "none";
-        pipe.classList.add("pipe");
-       
-        //Intervalo de adição de pontos no score.
-        interval = setInterval(scoreCounter, 150);
-    };
-});
+}
+//Intervalo de adição de pontos no score.
+interval = setInterval(scoreCounter, 250);
